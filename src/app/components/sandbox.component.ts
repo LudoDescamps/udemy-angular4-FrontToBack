d5fd19e69f2b6762195c85a23233e533bfcce8b0
ludo <atthevilla@free.fr>
2017-11-29 17:04:11 +0100

    Declaration des différents type

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -2,9 +2,21 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'sandbox',
-    template: `<h1>Hello World</h1>`
+    template: `<h1>Hello {{name}}</h1>`
 })
 
 export class SandboxComponent {
-    
+    // Déclaration des différents types en TypeScript
+    name: string = 'John Doe';
+    age: number = 35;
+    hasChildren: boolean = true;
+    city: any = 'Boston';
+    myNumbersArray: number[] = [1, 2, 3];
+    myStringsArray: string[] = ['hello', 'world'];
+    myAnyArray: any[] = [1, 2, 'hello', false];
+    myTuple: [string, number] = ['hello', 3];
+    unusable: void = undefined;
+    u: undefined = undefined;
+    n: null = null;
+
 }
