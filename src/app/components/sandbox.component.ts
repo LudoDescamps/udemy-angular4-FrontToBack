59e44f0e46a75db84b68e0945061031fa933c101
ludo <atthevilla@free.fr>
2017-11-29 19:16:01 +0100

    Etat avant début section 4

--- src/app/components/sandbox.component.ts
+++ src/app/components/sandbox.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+// Importe l'interface Customer.ts
+import { Customer } from './Customer';
+
 @Component({
     selector: 'sandbox',
     template: `<h1>Hello {{name}}</h1>`
 })
 
 export class SandboxComponent {
-    // Déclaration des différents types en TypeScript
+    /* // Déclaration des différents types en TypeScript
     name: string = 'John Doe';
     age: number = 35;
     hasChildren: boolean = true;
@@ -17,6 +20,37 @@ export class SandboxComponent {
     myTuple: [string, number] = ['hello', 3];
     unusable: void = undefined;
     u: undefined = undefined;
-    n: null = null;
+    n: null = null; */
 
+    // Crée l'objet customer
+    customer: Customer;
+    // Crée un array d'objets 
+    cutomers: Customer[];
+
+    constructor() {
+        this.customer = {
+            id: 1,
+            name: 'John Doe',
+            email: 'john.doe@email.com'
+        }
+
+        this.cutomers = [
+            {
+                id: 1,
+                name: 'John Doe',
+                email: 'john.doe@email.com'
+            },
+            {
+                id: 2,
+                name: 'Brad Traversy',
+                email: 'brad@email.com'
+            },
+            {
+                id: 3,
+                name: 'Steeve Smith',
+                email: 'steeve@email.com'
+            }
+
+        ]
+    }
 }
